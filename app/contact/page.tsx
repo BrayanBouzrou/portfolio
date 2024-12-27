d7acf8d5a6385f5ef628540058a22e4f26ea678f
BrayanBouzrou <126574260+BrayanBouzrou@users.noreply.github.com>
2024-12-27 01:13:25 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,7 +11,6 @@ export default function Contact() {
             <main className={styles.main}>
                 <h1>Contact</h1>
                 <p><strong>Email :</strong> bouzroubrayan@gmail.com</p>
-                <p><strong>Téléphone :</strong> 0620388175</p>
                 <p><strong>LinkedIn :</strong> <a href="https://www.linkedin.com/in/brayan-bouzrou" target="_blank">Mon profil LinkedIn</a></p>
             </main>
         </div>
